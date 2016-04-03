e06a0f1f9cb1763699be82617246a7d43bbfe4fb
George Matthews <gfmatthews@live.com>
2016-04-03 13:44:58 -0400

    Updating assembly information to have correct metadata

--- ImgShareData/Properties/AssemblyInfo.cs
+++ ImgShareData/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImgShareData")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ImgShare.APISource")]
+[assembly: AssemblyDescription("A simple asynchronous imgur client for Windows applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImgShareData")]
-[assembly: AssemblyCopyright("Copyright George Matthews © 2015")]
+[assembly: AssemblyCompany("gfmatthews")]
+[assembly: AssemblyProduct("ImgShare.APISource")]
+[assembly: AssemblyCopyright("Copyright George Matthews © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.6")]
-[assembly: AssemblyFileVersion("0.0.3.6")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
 [assembly: ComVisible(false)]
